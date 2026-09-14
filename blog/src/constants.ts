512d4a531f871fe22ea0f2b55dadc00c1943b535
Ronen <ronenamos@gmail.com>
2026-09-14 22:31:27 +0300

    Publish: gemini-keep and update footer social links

--- blog/src/constants.ts
+++ blog/src/constants.ts
@@ -1,5 +1,6 @@
 import type { Props } from "astro";
 import IconMail from "@/assets/icons/IconMail.svg";
+import IconYouTube from "@/assets/icons/IconYouTube.svg";
 import IconGitHub from "@/assets/icons/IconGitHub.svg";
 import IconBrandX from "@/assets/icons/IconBrandX.svg";
 import IconLinkedin from "@/assets/icons/IconLinkedin.svg";
@@ -18,34 +19,22 @@ interface Social {
 
 export const SOCIALS: Social[] = [
   {
-    name: "GitHub",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: `${SITE.title} on GitHub`,
-    icon: IconGitHub,
-  },
-  {
-    name: "X",
-    href: "https://x.com/username",
-    linkTitle: `${SITE.title} on X`,
-    icon: IconBrandX,
-  },
-  {
     name: "LinkedIn",
-    href: "https://www.linkedin.com/in/username/",
-    linkTitle: `${SITE.title} on LinkedIn`,
+    href: "https://www.linkedin.com/in/ronenamoscpa/",
+    linkTitle: `${SITE.title} ב-LinkedIn`,
     icon: IconLinkedin,
   },
   {
-    name: "Mail",
-    href: "mailto:yourmail@gmail.com",
-    linkTitle: `Send an email to ${SITE.title}`,
-    icon: IconMail,
+    name: "YouTube",
+    href: "https://www.youtube.com/@AIFinanceTransformation",
+    linkTitle: `${SITE.title} ב-YouTube`,
+    icon: IconYouTube,
   },
   {
-    name: "Telegram",
-    href: "https://t.me/AI_World_IL",
-    linkTitle: `${SITE.title} on Telegram`,
-    icon: IconTelegram,
+    name: "Mail",
+    href: "mailto:ronenamos@gmail.com",
+    linkTitle: `שליחת אימייל ל-${SITE.title}`,
+    icon: IconMail,
   },
 ] as const;
 
